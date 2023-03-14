c49833c99593f065e302c08b10f5cf08002198d1
alok <alokdebnath.in@gmail.com>
2023-03-14 12:28:35 +0530

    Initian upload to git

---
+++ script.js
@@ -0,0 +1,15 @@
+var overlay = document.querySelector("#hover-overlay");
+var img = document.querySelector("#img");
+var img_container = document.querySelector("#img-container");
+
+overlay.addEventListener("mouseover", function () {
+	img_container.classList.add("hover");
+    overlay.classList.add("anti-hover");
+    img.classList.add("anti-hover");
+});
+
+overlay.addEventListener("mouseout", function () {
+	img_container.classList.remove("hover");
+    overlay.classList.remove("anti-hover");
+    img.classList.remove("anti-hover");
+});
