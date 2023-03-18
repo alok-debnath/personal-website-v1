925a80da53084d1acbda192891433e69c91cd2b0
alok <alokdebnath.in@gmail.com>
2023-03-18 15:31:46 +0530

    Added "about me" and few changes to the self photo

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ var img_container = document.querySelector("#img-container");
 
 overlay.addEventListener("mouseover", function () {
 	img_container.classList.add("hover");
-    overlay.classList.add("anti-hover");
-    img.classList.add("anti-hover");
+	overlay.classList.add("anti-hover");
+	img.classList.add("anti-hover");
 });
 
 overlay.addEventListener("mouseout", function () {
 	img_container.classList.remove("hover");
-    overlay.classList.remove("anti-hover");
-    img.classList.remove("anti-hover");
+	overlay.classList.remove("anti-hover");
+	img.classList.remove("anti-hover");
 });
